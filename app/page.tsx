66e0df676a826914468cd25f3eca9de2a5cc668e
EstefaniaCabal <estefy.cabal@gmail.com>
2024-01-16 12:27:23 -0600

    actuali

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,39 @@
-import Link from 'next/link';
+// import Head from "next/head";
+import styles from "@/styles/styles.module.css"
+import Footer from "@/components/footer"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <header>
-        <h1 className="text-3xl font-bold mb-4">Bienvenido a mi aplicación</h1>
-        <img src="https://th.bing.com/th/id/R.13cbed9c1204328ff11e93accee627b5?rik=oypWnGLoYGy1XA&riu=http%3a%2f%2f4.bp.blogspot.com%2f-KqIn9k3g9ck%2fUTsdEH8WotI%2fAAAAAAAAAHY%2fKBTqD5kiGWw%2fs1600%2fanimals_beautiful_extraordinary_wild_birds_mad_owl_picture-17.jpg&ehk=PaRRYt7vDNXiCq2ArmqleK%2bR5dBMXkaVo94zD5EfWg0%3d&risl=&pid=ImgRaw&r=0" />
-      </header>
+      <div className={styles.container}>
+      {/*  <Head>
+          <title>MI PAGINA WEB</title>
+          <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+          <meta name="viewport" content="width-device-width, initial-scale=1, shrink-to-fit-no"/>
+        </Head > */}
 
-      <nav className="mb-8">
-        <ul className="flex space-x-4">
-          <li>
-            <Link href="/" passHref>
-              Home
-            </Link>
-            
-          </li>
-          <li>
-            <Link href="/pages/test" passHref>
-                Test
-            </Link>
-          </li>
-        </ul>
-      </nav>
+        {/* NAVBAR*/}
+        <nav className={styles.navbar}>
+          <a href="#" className={styles.navbarBrand}> HOME </a>
+          <ul className={styles.navbarNav}>
+            <li className={styles.navbarItem}>
+              <a href="#" className={styles.navLink}>Ayuda</a>
+            </li>
+            <li className={styles.navbarItem}>
+              <a href="#" className={styles.navLink}>  Sesion</a>
+            </li>
+          </ul>
+        </nav>
 
-      <footer className="text-center">
-        <p className="text-sm text-gray-500">
-          &copy; 2024 Derechos Reservados | Desarrollado por Estefanía Cabal - Grupo TI02SM22
-        </p>
-      </footer>
-    </main>
-  );
-}
+        {/*CONTENIDO PRINCIPAL*/}
+        <div className={styles.content}>
+          <h1>Hola mundo</h1>
+        </div>
 
+        {/*Componente footer*/}
+        <Footer>
 
+        </Footer>
 
+      </div>
+    )
+  }
